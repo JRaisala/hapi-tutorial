3851ac7415677d85f20a8365f9f66b46893d10ee
Juha Räisälä <juha.raisala@gmail.com>
2019-02-07 15:35:43 +0200

    Add users profile editing

--- models/user.js
+++ models/user.js
@@ -8,8 +8,19 @@ const When = require("when");
 const MD5 = require("md5");
 const Crypto = require("crypto");
 const Validator = require("validator");
+const _ = require('lodash')  
+const Haikunator = require('haikunator');
+
 
 const SALT_WORK_FACTOR = 12;
+
+// initialize haikunator to generate funky usernames
+// initialize to not append numbers to generated names
+// e.g. "proud-mode" instead of "proud-mode-4711"
+const haikunator = new Haikunator({  
+	defaults: { tokenLength: 0 }
+  })
+  
 
 const userSchema = new Schema({
   email: {
@@ -32,8 +43,42 @@ const userSchema = new Schema({
     unique: true,
     trim: true
   },
-  passwordResetDeadline: Date
+  passwordResetDeadline: Date,
+
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    sparse: true // this makes sure the unique index applies to not null values only (= unique if not null)
+  },
+  homepage: {
+    type: String,
+    trim: true
+  }
+
 });
+
+
+/**
+ * pre-save hook to generate a unique username
+ */
+userSchema.pre('save', async function(next) {  
+	if (this.isNew) {
+	  // split email address at @ symbol and only return the first part
+	  this.username = _.first(_.split(this.email, '@', 1))
+  
+	  // find existing user with the same username
+	  const existingUser = await this.constructor.findOne({ username: this.username })
+  
+	  // TODO this can lead to duplicated keys if haikunate generates the same slug twice
+	  if (existingUser) {
+		this.username = `${this.username}-${haikunator.haikunate()}`
+	  }
+	}
+  
+	// tell mongoose to proceed
+	next()
+  })
 
 // Static method for finding email
 userSchema.statics.findByEmail = function(email) {
